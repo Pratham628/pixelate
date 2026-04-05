2d2c74690f535a71c49199b12e50ed76b7e39dea
Pratham628 <rajput.pp2007@gmail.com>
2026-04-05 16:29:31 +0530

    my commit

--- task1/template.cpp
+++ task1/template.cpp
@@ -26,9 +26,40 @@ int main()
 
     cv::Mat img_f;
     img.convertTo(img_f, CV_64FC1);
+    
+    filter2D(input, output, -1, gaussian);              // Convolve with original 'full' kernel
 
-    // TODO: apply K1 and K2 to img_f with as few multiplications per pixel as possible.
-    // Print the number of multiplications your approach uses per pixel.
+    // Making All Windows resizable
+    cv::namedWindow("Output", cv::WINDOW_NORMAL);
+    cv::imshow("Output", output);
+    cv::waitKey(0); // Wait for key press before moving to the next image
+
+    // Define decomposed standard 3x3 Gaussian kernel
+    cv::Mat K1_v = (cv::Mat_<double>(3, 1) << 1,0,-1);
+    cv::Mat K1_h = (cv::Mat_<double>(1, 3) << 1,2,1);
+
+    cv::Mat K1_h = (cv::Mat_<double>(1, 3) << 1,2,1);
+    cv::Mat K1_v = (cv::Mat_<double>(3, 1) << 1,0,-1);
+    
+    // Declare matrix to store intermediate image
+    cv::Mat intermediate;
+
+    // Now to actually convolve
+    intermediate = convolve(input,  K1_v);         // Convolve Vertically
+    output = convolve(intermediate, K1_h);        // Convolve Horizontally
+
+    cv::namedWindow("Output by Naive Seperable Convolution", cv::WINDOW_NORMAL);
+    cv::imshow("Output by Naive Seperable Convolution", output);
+    cv::waitKey(0); // Wait for key press before moving to the next image
+
+    cv::namedWindow("Output of Vertical Convolution", cv::WINDOW_NORMAL);
+    cv::imshow("Output of Vertical Convolution", intermediate);
+    cv::waitKey(0); // Wait for key press before moving to the next image
+
+    cv::namedWindow("Output of Horizontal Convolution", cv::WINDOW_NORMAL);
+    cv::imshow("Output of Horizontal Convolution", output);
+    cv::waitKey(0); // Wait for key press before exiting
+
 
     return 0;
 }
